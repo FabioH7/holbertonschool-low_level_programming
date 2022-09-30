f1175330a80fbe1f5d7cc7b14f8be1489e0b5786
FabioH7 <fhysollari4@gmail.com>
2022-09-30 02:48:40 -0700

    printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+char MyStr[] = "with proper grammar, but the outcome is a piece of art,\n";
+printf("%s", MyStr);
+return (0);
+}
