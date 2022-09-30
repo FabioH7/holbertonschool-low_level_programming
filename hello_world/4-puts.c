ee89afa6f16af087fb55c6f2839950a958e6c20b
FabioH7 <fhysollari4@gmail.com>
2022-09-30 00:38:15 -0700

    using puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-	char MyStr[] = "\"Programming is like building a multilingual puzzle";
+	char MyStr[] = "\"Programming is like building a multilingual puzzle\n";
 	puts(MyStr);
-	return 0;
+	return (0);
 }
