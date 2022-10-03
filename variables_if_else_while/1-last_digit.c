7e970dc7c65dd8dbc4761ae5205adc8546582eb8
FabioH7 <fhysollari4@gmail.com>
2022-10-03 06:19:03 -0700

    last digit of rando num done

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - is where the function starts
+ * Return: shows that the function works
+ */
+int main(void)
+{
+	int n;
+	int num;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	num = n % 10;
+	if (num > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, num);
+	else if (num == 0)
+		printf("Last digit of %d is %d and is 0\n", n, num);
+	else
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, num);
+	return (0);
+}
