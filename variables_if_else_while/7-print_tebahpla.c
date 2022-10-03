cb680329a41f50aff220fe81385bada5222e0fe6
FabioH7 <fhysollari4@gmail.com>
2022-10-03 08:10:03 -0700

    tebahpla tnrip

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - is where the function starts
+ * Return: shows that the function works
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'z'; x <= 'a'; x--)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
