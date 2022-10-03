d64da36fb096c502b8f7eccf42095b408bdf3b69
FabioH7 <fhysollari4@gmail.com>
2022-10-03 10:43:31 -0700

    print 3 digits

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - this is the entry point of the program
+ * Return: returns 0 if sucess
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = 1; j <= 8; j++)
+		{
+			for (k = 2; k <= 9; k++)
+			{
+			if (i != j && i < j && j < k)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				if (i + j + k != 24)
+					{
+					putchar(',');
+					putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
