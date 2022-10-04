b9092a93041f6fb3ed63ad8f6c9ddd95d5ca1e72
FabioH7 <fhysollari4@gmail.com>
2022-10-04 02:44:07 -0700

    task 1 done

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -8,11 +8,14 @@
 
 int main(void)
 {
-	char MyStr[] = "_putchar";
-	int i;
-
-	for (i = 0; i != '\0'; i++)
-		_putchar(MyStr[i]);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
