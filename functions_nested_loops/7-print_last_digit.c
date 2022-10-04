ac3496b4c2aa1070e886f9daee61b0d1c10df162
FabioH7 <fhysollari4@gmail.com>
2022-10-04 10:04:52 -0700

    Franc Dushi

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_last_digit - main func
+ * Return: 0 if successful
+ * @n: parameter of the function
+ */
+
+int print_last_digit(int n)
+{
+	int m = n % 10;
+
+	if (m < 0)
+		m = m * -1;
+	_putchar(m + '0');
+	return (0);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -5,5 +5,6 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
+int print_last_digit(int);
 void print_alphabet(void);
 int _putchar(char c);
