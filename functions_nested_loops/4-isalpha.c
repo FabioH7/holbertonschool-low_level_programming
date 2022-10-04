7020f72137b82e4de0117429fff51e159413153a
FabioH7 <fhysollari4@gmail.com>
2022-10-04 06:08:09 -0700

    is aplha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _isalpha - main func
+ * Return: 0 if successful
+ * @c: parameter of the function
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' && c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,5 +2,6 @@
 
 void print_alphabet_x10(void);
 int _islower(int c);
+int _isalpha(int c);
 void print_alphabet(void);
 int _putchar(char c);
