6424e5ef74acaa50344634691016c2832bfd701a
FabioH7 <fhysollari4@gmail.com>
2022-10-04 05:18:21 -0700

    alph x 10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -9,8 +9,12 @@
 void print_alphabet_x10(void)
 {
 	int i;
+	int letter;
 
 	for (i = 0; i <= 9; i++)
-		print_alphabet();
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
 	_putchar('\n');
+	}
 }
