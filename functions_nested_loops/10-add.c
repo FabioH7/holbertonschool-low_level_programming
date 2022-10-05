11de3382fdc06e54e08656ec7d13d4e70a88ec0e
FabioH7 <fhysollari4@gmail.com>
2022-10-05 05:54:11 -0700

    adds 2 ints

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * add - main func
+ * @x: first int
+ * @y: first int
+ * Return: sum of int x and int y
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -8,5 +8,6 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
+int add(int, int);
 void print_alphabet(void);
 int _putchar(char c);
