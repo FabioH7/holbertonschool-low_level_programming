771200c7a9908a8b0ab14c99c331a16dd094a703
FabioH7 <fhysollari4@gmail.com>
2022-10-05 01:50:14 -0700

    Jacku

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * jack_bauer - main func
+ * Return: 0 if successful
+ */
+
+void jack_bauer(void)
+{
+	int hour, min;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar(hour / 10 + '0');
+			_putchar(hour % 10 + '0');
+			_putchar(':');
+			_putchar(min / 10 + '0');
+			_putchar(min % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
