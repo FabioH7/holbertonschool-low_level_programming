782fe87e203dc9614244ae4c2647cd43397e4fc3
FabioH7 <fhysollari4@gmail.com>
2022-10-06 01:48:42 -0700

    print most numbers done

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -4,6 +4,7 @@ int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+void print_most_numbers(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
