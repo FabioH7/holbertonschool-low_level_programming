d970edb65e707a99ca0a6e7fed3ea293842c5480
FabioH7 <fhysollari4@gmail.com>
2022-10-06 01:19:38 -0700

    multiplication done

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * mul - main function
+ * @a: first parameter
+ * @b: second parameter
+ * Return: 1 if uppercase and 0 if false
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -2,6 +2,7 @@
 
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
