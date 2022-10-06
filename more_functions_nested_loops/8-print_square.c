4529b951a8f03e08177f5b3529d15e131dda9f1c
FabioH7 <fhysollari4@gmail.com>
2022-10-06 05:21:37 -0700

    square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_square - main function
+ * @size: parameter
+ * Return: 1 if uppercase and 0 if false
+ */
+
+void print_square(int size)
+{
+	int diagonal, horisontal;
+
+	for (diagonal = 0; diagonal < size; diagonal++)
+	{
+		for (horisontal = 0; horisontal < size; horisontal++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
