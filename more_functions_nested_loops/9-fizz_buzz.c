c766c3ff0d2eaed31ca05dc5d446420b96c52364
FabioH7 <fhysollari4@gmail.com>
2022-10-07 00:57:53 -0700

    buzz buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -2,13 +2,22 @@
 #include <unistd.h>
 int main (void)
 {
-	int num;
-	
-	for (num = 0; num < 100; num ++)
+	int count;
+
+	for (count = 1; count <= 100; count++)
 	{
-		for (num = 1; num < 101; num ++)
-			printf(" ");
-	printf("%d", num);
+		if (count % 15 == 0)
+			printf("FizzBuzz");
+		else if (count % 5 == 0)
+			printf("Buzz");
+		else if (count % 3 == 0)
+			printf("Fizz");
+		else 
+			printf("%d", count);
+		if (count == 100)
+			continue;
+		printf(" ");
 	}
 	printf("\n");
+	return(0);
 }
