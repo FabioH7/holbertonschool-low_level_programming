3bc0ece6fed3e724088f7eddc76383bdadc33d77
FabioH7 <fhysollari4@gmail.com>
2022-10-10 05:28:25 -0700

    print len

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - main function
+ * Return: returns i
+ * @s: param
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
