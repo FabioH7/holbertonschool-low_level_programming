121dccffb52d9683cd68c2efff15eae4cd8127fa
FabioH7 <fhysollari4@gmail.com>
2022-10-11 01:36:13 -0700

    print every 2 indexes of a string

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - main function
+ * @str: param
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
