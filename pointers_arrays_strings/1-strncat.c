443d27e3f8ce0fc830edfb8923087be02503cacf
FabioH7 <fhysollari4@gmail.com>
2022-10-11 07:27:18 -0700

    concatenate some string

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcat - main function
+ * @dest: param
+ * @src: param
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j;
+
+	while (dest[i] != 0)
+	{
+		i++;
+	}
+	for (j = 0; src[j] != 0 && j < n; j++)
+		dest[i + j] = src[j];
+	dest[i + j] = '\0';
+	return (dest);
+}
