c517da881123345de715abbd6ab2c616e46e78c9
FabioH7 <fhysollari4@gmail.com>
2022-10-12 07:02:36 -0700

    print all uppercase

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -5,14 +5,14 @@
  * Return: returns sum
  * @string: param
  */
-char *string_toupper(char *string);
+char *string_toupper(char *string)
 {
 	int i;
 
 	for (i = 0; string[i] != '\0'; i++)
 	{
 		if (string[i] >= 'a' && string[i <= 'b'])
-			string[i] -= 32
+			string[i] -= 32;
 	}
-	return (string)
+	return (string);
 }
