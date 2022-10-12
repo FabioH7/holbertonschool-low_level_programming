676f475a92ca71322a28847b22d11ab09b7951e8
FabioH7 <fhysollari4@gmail.com>
2022-10-12 07:00:49 -0700

    print all uppercase

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * string_toupper - main function
+ * Return: returns sum
+ * @string: param
+ */
+char *string_toupper(char *string);
+{
+	int i;
+
+	for (i = 0; string[i] != '\0'; i++)
+	{
+		if (string[i] >= 'a' && string[i <= 'b'])
+			string[i] -= 32
+	}
+	return (string)
+}
