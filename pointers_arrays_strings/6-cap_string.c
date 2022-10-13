53624fccaa4d6fc4d20fc9860c5b2a7d549e94de
FabioH7 <fhysollari4@gmail.com>
2022-10-13 01:06:10 -0700

    cap string function done

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,24 +1,32 @@
 #include "main.h"
-
 /**
- * string_toupper - main function
- * Return: returns sum
- * @string: param
+ * cap_string - main func
+ *
+ * @str: is a parameter
+ * Return: return 1
  */
-char *cap_string(char *string)
+char *cap_string(char *str)
 {
 	int i = 0;
 
-	if (string[i] >= 'a' && string[i <= 'b'])
-		string[i] -= 32;
-	for (i = 0; string[i] < '\n'; i++)
+	if (str[i] >= 'a' && str[i] <= 'z')
+		str[i] -= 32;
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (string[i] == ' ' || string[i] == ',' || string[i] == ';' || string[i] == '.' || string[i] == '!' || string[i] == '\n' || string[i] == '\t' || string[i] == '?' || string[i] == '"' || string[i] == '(' || string[i] == ')' || string[i] == '{' || string[i] == '}')
+		if (str[i] > 0 && str[i] < 47 && str[i] != '-')
 		{
 			i++;
-			if (string[i] >= 'a' && string[i <= 'b'])
-				string[i] -= 32;
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] -= 32;
+			i--;
+		}
+		if (str[i] > 122 || (str[i] > 57 && str[i] < 65))
+		{
+			i++;
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] -= 32;
+			i--;
 		}
 	}
-	return (string);
+	return (str);
 }
