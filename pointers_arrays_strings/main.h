0fa8de082a95cd3c7cc74c0e675608b021dfdfc7
FabioH7 <fhysollari4@gmail.com>
2022-10-14 08:14:49 -0700

    chessboard print test

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -24,3 +24,4 @@ char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
 unsigned int _strspn(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
