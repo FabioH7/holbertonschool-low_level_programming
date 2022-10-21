e017a193b871829a21138592a525cea48cc3d380
FabioH7 <fhysollari4@gmail.com>
2022-10-21 02:17:36 -0700

    print argc done

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * @argc: parameter number
+ * @argv: all parameters
+ * Return: returns 0 if true
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
