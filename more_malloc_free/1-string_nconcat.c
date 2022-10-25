553e4b857507466739b2bee7b0be03714a91a579
FabioH7 <fhysollari4@gmail.com>
2022-10-25 04:49:22 -0700

    concatenate 2 string n bytes

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -15,26 +15,18 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *pnt;
 	size_t i = 0, j = 0;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	pnt = malloc(sizeof(char) * _strlen(s1) + _strlen(s2) + 1);
 	if (pnt == NULL)
 		return (NULL);
-	if (n >= _strlen(s1))
+	while (*s1 != '\0')
 	{
-		while (*s1 != '\0')
-		{
-			pnt[i] = *s1;
-			s1++;
-			i++;
-		}
-	}
-	else
-	{
-		while (*s1 != '\0')
-		{
-			pnt[i] = *s1;
-			s1++;
-			i++;
-		}
+		pnt[i] = *s1;
+		s1++;
+		i++;
 	}
 	if (n >= _strlen(s2))
 	{
