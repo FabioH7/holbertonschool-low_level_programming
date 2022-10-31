c942252b27fbc7b487532223ecabc6ebba7afd98
FabioH7 <fhysollari4@gmail.com>
2022-10-31 07:21:19 -0700

    test 2.0

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -9,6 +9,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newd;
 	int i, len1, len2;
+	char *cpn, *cpo;
 
 	for (i = 0; name[i] != '\0'; i++)
 	{
@@ -21,19 +22,27 @@ dog_t *new_dog(char *name, float age, char *owner)
 	newd = malloc(sizeof(dog_t));
 	if (newd == NULL)
 		return (NULL);
-	newd->name = malloc(sizeof(char) * len1 + 1);
-	if (newd->name == NULL)
+	cpn = malloc(sizeof(char) * len1 + 1);
+	if (cpn == NULL)
 	{
 		free(newd);
 	}
-	newd->owner = malloc(sizeof(char) * len2 + 1);
-	if (newd->owner == NULL)
+	cpo = malloc(sizeof(char) * len2 + 1);
+	if (cpo == NULL)
 	{
-		free(newd->name);
+		free(cpo);
 		free(newd);
 	}
-	newd->name = name;
+	for (i = 0; name[i] != '\0'; i++)
+	{
+		cpn[i] = name[i];
+	}
+	for (i = 0; owner[i] != '\0'; i++)
+	{
+		cpo[i] = name[i];
+	}
+	newd->name = cpn;
 	newd->age = age;
-	newd->owner = owner;
+	newd->owner = cpo;
 	return (newd);
 }
