e0a42bff50345232e7516ffa1448f781d032b78e
FabioH7 <fhysollari4@gmail.com>
2022-10-31 07:09:16 -0700

    small test

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * print_dog - initialise function
+ * @d: struct
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newd;
+	int i, len1, len2;
+
+	for (i = 0; name[i] != '\0'; i++)
+	{
+	}
+	len1 = i;
+	for (i = 0; owner[i] != '\0'; i++)
+	{
+	}
+	len2 = i;
+	newd = malloc(sizeof(dog_t));
+	if (newd == NULL)
+		return (NULL);
+	newd->name = malloc(sizeof(char) * len1 + 1);
+	if (newd->name == NULL)
+	{
+		free(newd);
+	}
+	newd->owner = malloc(sizeof(char) * len2 + 1);
+	if (newd->owner == NULL)
+	{
+		free(newd->name);
+		free(newd);
+	}
+	newd->name = name;
+	newd->age = age;
+	newd->owner = owner;
+	return (newd);
+}
