e2aec8c7147c9cff74f1a3b48c5c74a372d93ad6
FabioH7 <fhysollari4@gmail.com>
2022-10-31 03:32:25 -0700

    dog structure

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
