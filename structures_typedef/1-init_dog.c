47abaa37eb5341946f35aa1ae60f3d4c526b58f3
FabioH7 <fhysollari4@gmail.com>
2022-10-31 04:19:55 -0700

    intialise var of dog

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,9 +1,18 @@
 #include <stdlib.h>
 #include "dog.h"
-
+/**
+ * init_dog - initialise function
+ * @d: struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner name
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d)
+	{
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
+	}
 }
