bc80437b0b726d4e9f860aa831bd389f99617dd7
FabioH7 <fhysollari4@gmail.com>
2022-11-01 05:30:28 -0700

    print index

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+ * int_index - function
+ * @array: array to search
+ * @size: array size
+ * @cmp: pointer to function
+ * Return: index
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) == 1)
+			{
+				return (i);
+			}
+		}
+	}
+	return (-1);
+}
