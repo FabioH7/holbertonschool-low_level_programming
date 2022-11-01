36fdf38f2868283ec5646dce42dcef31ee5fa240
FabioH7 <fhysollari4@gmail.com>
2022-11-01 04:53:28 -0700

    array iterator print

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+ * array_iterator - function
+ * @array: array to be printed
+ * @size: array size
+ * @action: pointer to function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (action)
+		for (i = 0; i < size; i++)
+			action(array[i]);
+}
