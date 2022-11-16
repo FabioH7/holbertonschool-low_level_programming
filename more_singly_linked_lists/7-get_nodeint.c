6fe39f9571e6f527fe44c3cb55c95c6ee4c7eaf3
FabioH7 <fhysollari4@gmail.com>
2022-11-15 23:34:11 -0800

    get nth node of list

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -19,6 +19,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	while  (count != index)
 	{
 		temp = temp->next;
+		if (temp == NULL)
+			return (NULL);
 		count++;
 	}
 	return (temp);
