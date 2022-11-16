8bb3614d16af2a62f7ca0886a7cb63671ad2d1e8
FabioH7 <fhysollari4@gmail.com>
2022-11-16 02:04:40 -0800

    insert node nth postion

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -21,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	pnt = *head;
 	if (idx == 0)
 	{
-		pnt->next = *head;
+		temp->next = *head;
 		*head = temp;
 		return (*head);
 	}
