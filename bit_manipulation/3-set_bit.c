9482bfd0a8d4790a3aef16dc00983897bc00fdbb
FabioH7 <fhysollari4@gmail.com>
2022-11-17 05:46:43 -0800

    set bit to 1

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * set_bit - set nth bit of int as 1
+ * @n: number to find bit
+ * @index: where to get the bit
+ * Return: bit value or -1 if it fails
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 32)
+	{
+		return (-1);
+	}
+	if (n != 0)
+	{
+		*n = *n | (1 << index);
+		return (1);
+	}
+	return (-1);
+}
