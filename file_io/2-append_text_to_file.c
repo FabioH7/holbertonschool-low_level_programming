6a58de7d0c6f49b390a9a308acf8d21b674bf8ed
FabioH7 <fhysollari4@gmail.com>
2022-11-21 06:32:56 -0800

    append text to file func

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -16,6 +16,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_APPEND | O_CREAT, 0600);
 	if (fd == -1)
 		return (-1);
+	if (text_content == NULL)
+		return (1);
 	file_w = write(fd, text_content, strlen(text_content));
 	if (file_w == -1)
 		return (-1);
