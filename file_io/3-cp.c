8a473fdbc94e2c551a406347f8158fad0b9625dc
FabioH7 <fhysollari4@gmail.com>
2022-11-22 02:43:47 -0800

    test test

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -32,7 +32,7 @@ int main (int argc, char *argv[])
                 dprintf(2, "Error: Can't write to %s\n", argv[2]);
                 exit(99);
         }
-        while ((file_read = read(file_from,buff,file_from_size)) > 0)
+        while ((file_read = read(file_from,buff,file_from_size)) > 1)
         {
                 if (file_read == -1)
                 {
