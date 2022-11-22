79aa290c5509f71a8f521aa6417be72b6d5e5c4b
FabioH7 <fhysollari4@gmail.com>
2022-11-22 01:42:40 -0800

    cp test

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -23,13 +23,13 @@ int main (int argc, char *argv[])
         file_from = open(argv[1], O_RDONLY);
         if (file_from == -1)
         {
-                dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+                dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", argv[1]);
                 exit(98);
         }
         file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
         if (file_to == -1)
         {
-                dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", argv[2]);
+                dprintf(2, "Error: Can't write to %s\n", argv[2]);
                 exit(99);
         }
         while ((file_read = read(file_from,buff,file_from_size)) > 0)
