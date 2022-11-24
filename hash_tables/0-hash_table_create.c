2f7e207108930d7e3295d5b525ef641c39ac980b
FabioH7 <fhysollari4@gmail.com>
2022-11-24 04:00:23 -0800

    create hash table

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,5 +1,10 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - create a hash table
+ * @size: table size
+ * Return: table
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
