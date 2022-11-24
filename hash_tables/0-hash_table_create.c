411f2a2ff61cea0464bda154b4af1091606fdcd5
FabioH7 <fhysollari4@gmail.com>
2022-11-24 04:01:10 -0800

    create hash table

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -15,6 +15,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table->size = size;
 	table->array = malloc(sizeof(hash_node_t *) * size);
 	if (table->array == NULL)
-		return NULL;
+		return (NULL);
 	return (table);
 }
