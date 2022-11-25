73fa3ddba6a4af511f0acade628e0482f24448c8
FabioH7 <fhysollari4@gmail.com>
2022-11-25 03:41:27 -0800

    fixed betty cp

--- file_io/main.h
+++ file_io/main.h
@@ -6,6 +6,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+void cp(char *file_from, char *file_to);
+void cls(int file);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
