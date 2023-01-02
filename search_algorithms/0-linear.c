46b3d28625a0334188dcc6cf6e6a1fce5e2207e0
FabioH7 <fhysollari4@gmail.com>
2023-01-02 04:56:24 -0800

    adding first algorithm

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,14 @@
+#include "search_algos.h"
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%li] = [%i]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
